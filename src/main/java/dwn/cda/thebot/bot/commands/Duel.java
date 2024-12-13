d50d2dc64197ed1dd7d1146860f99c67170d4186
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-13 15:51:28 +0100

    feat: shuffle winner + anounce

--- src/main/java/dwn/cda/thebot/bot/commands/Duel.java
+++ src/main/java/dwn/cda/thebot/bot/commands/Duel.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Duel {
     /*
@@ -15,12 +16,25 @@ public class Duel {
     *   fonction qui fait le 50/50 et qui retourne le resultat
     * */
 
+
     public static void handleDuelCommand(SlashCommandInteractionEvent event) {
         Member member = event.getMember(); // celui qui va défier
         Member opponent = event.getOption("opponent").getAsMember();
 
-        event.reply("ID :"+member.getId() + " Name "  +  member.getEffectiveName() + "ID :"+opponent.getId() + " Name "  +  opponent.getEffectiveName()).setEphemeral(true).queue();
+        Member winner = shuffle(member,opponent);
+
+
+        event.reply("Le gagnant est : " + winner.getEffectiveName()).setEphemeral(true).queue();
     }
 
+    public static Member shuffle (Member member, Member opponent) {
+        Random random = new Random();
+        boolean result = random.nextBoolean();
+        if (result == false) {
+            return opponent;
+        } else {
+            return member;
+        }
+    }
 
 }
