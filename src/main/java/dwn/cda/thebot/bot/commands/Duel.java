f68c404aaa4e61d64b54ba1b4a388d7dd9f49a51
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-13 15:35:01 +0100

    feat: ajout opposent

--- src/main/java/dwn/cda/thebot/bot/commands/Duel.java
+++ src/main/java/dwn/cda/thebot/bot/commands/Duel.java
@@ -19,10 +19,7 @@ public class Duel {
         Member member = event.getMember(); // celui qui va défier
         Member opponent = event.getOption("opponent").getAsMember();
 
-        event.reply("ID :"+member.getId() + " Name "  +  member.getEffectiveName()).setEphemeral(true).queue();
-
-        event.reply("ID :"+opponent.getId() + " Name "  +  opponent.getEffectiveName()).setEphemeral(true).queue();
-
+        event.reply("ID :"+member.getId() + " Name "  +  member.getEffectiveName() + "ID :"+opponent.getId() + " Name "  +  opponent.getEffectiveName()).setEphemeral(true).queue();
     }
 
 
